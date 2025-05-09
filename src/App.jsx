3558d70fd02b8aa3a4ea8998c022213a8f53babc
HaojieZhang123 <haojiezhang123@gmail.com>
2025-05-09 16:24:57 +0200

    [UPD] added list of article titles

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
+const articles = [
+  'Articolo 1',
+  'Articolo 2',
+  'Articolo 3',
+  'Articolo 4',
+  'Articolo 5',
+  'Articolo 6',
+  'Articolo 7',
+  'Articolo 8',
+  'Articolo 9',
+  'Articolo 10'
+]
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Blog</h1>
+      <ul>
+        {articles.map((article, index) => (
+          <li key={index}>{article}</li>
+        ))}
+      </ul>
     </>
   )
 }
